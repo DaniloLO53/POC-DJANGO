07344e0be036decdbc5538a8c81a4948e607e1f7
Danilo Leão <olin_ad@hotmail.com>
2023-06-05 09:29:34 -0300

    feat: route /other

--- src/poc_django/urls.py
+++ src/poc_django/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('polls/', include('polls.urls')),
+    path('other/', include('other.urls')),
     path('admin/', admin.site.urls),
 ]
